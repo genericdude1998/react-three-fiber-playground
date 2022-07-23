dff6d5574103a8a9f98ae42449ceafa9a7bf5935
vsenzo <vsenzo@hello.com>
2022-07-23 12:38:10 +0100

    add eslint

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Viewer from './Viewer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Viewer />,
+);
