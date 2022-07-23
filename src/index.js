0bfa7b75615d392b0bd553bc42aa8b04b411cd70
vsenzo <vsenzo@hello.com>
2022-07-23 12:21:13 +0100

    setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { Canvas } from '@react-three/fiber';
+import Box from './Box';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Canvas>
+  <ambientLight />
+  <pointLight position={[10, 10, 10]} />
+  <Box position={[-1.2, 0, 0]} />
+  </Canvas>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
