b1c55c80d52e1ee77c86928ec9255c7a46b003e6
vsenzo <vsenzo@hello.com>
2022-07-30 12:43:45 +0100

    add light intensity

--- src/Viewer.jsx
+++ src/Viewer.jsx
@@ -25,7 +25,7 @@ export default function Viewer({ tabValue }) {
     <CanvasWrapper>
       <Canvas>
         <Camera />
-        <pointLight position={[10, 10, 10]} />
+        <pointLight position={[10, 10, 10]} intensity={5} />
         <Box position={[0, 0, 0]} material={material} />
       </Canvas>
       ,
